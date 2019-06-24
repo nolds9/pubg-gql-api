386b4495212694961459a81bc9f9264141d929c6
Nicholas Olds <nolds@costar.com>
2019-06-24 14:43:56 -0400

    Add initial api & setup

---
+++ index.js
@@ -0,0 +1,59 @@
+const fetch = require("node-fetch");
+const { ApolloServer, gql } = require("apollo-server");
+require("dotenv").config();
+
+const baseUrl = "https://api.pubg.com/shards/steam/";
+
+const typeDefs = gql`
+  type Query {
+    getAccountId(username: String!): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getAccountId
+  }
+};
+
+async function getAccountId(_, { username = "" }) {
+  if (!username) {
+    throw new Error("Must provide username");
+  }
+
+  if (!process.env.PUBG_API_KEY) {
+    throw new Error("Must provide valid api key");
+  }
+
+  const url = `${baseUrl}players?filter[playerNames]=${username}`;
+  const headers = {
+    accept: "application/vnd.api+json",
+    Authorization: `Bearer ${process.env.PUBG_API_KEY}`
+  };
+
+  const response = await fetch(url, {
+    method: "get",
+    headers
+  }).then(res => res.json());
+
+  if (
+    !response ||
+    !response.data ||
+    !response.data.length ||
+    !response.data[0]
+  ) {
+    throw new Error(
+      "Request failed: bad request or no data for provided username"
+    );
+  }
+
+  const player = response.data[0];
+
+  return player.id || "";
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
